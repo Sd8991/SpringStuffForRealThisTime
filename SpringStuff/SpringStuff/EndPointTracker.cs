546e9aaf6e9a68c21bdff5a2b22e4e565055e630
Sjors Derksen <sjors.derksen@hotmail.nl>
2018-03-15 11:04:31 +0100

    Alles

---
+++ SpringStuff/SpringStuff/EndPointTracker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class EndPointTracker
+{
+    public Spring s;
+    public Dictionary<Vector2, int> points = new Dictionary<Vector2, int>();
+    public EndPointTracker(Spring s, Dictionary<Vector2, int> points)
+    {
+        this.s = s;
+        this.points = points;
+    }
+
+    public void Track()
+    {
+        List<Vector2> toDelete = new List<Vector2>();
+        foreach (Vector2 p in points.Keys.ToList())
+        {            
+            points[p]++;
+            if (points[p] > 50) toDelete.Add(p);
+        }
+        foreach (Vector2 d in toDelete)
+            points.Remove(d);        
+        points.Add(new Vector2((float)s.endPointX, (float)s.endPointY), 0);
+    }
+
+}
+
